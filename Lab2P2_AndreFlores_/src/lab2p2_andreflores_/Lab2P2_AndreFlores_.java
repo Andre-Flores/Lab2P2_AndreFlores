840bb8aea2d3d4c112077a820ab0fffc403d1df6
Andre-Flores <andrefloresromero@gmail.com>
2024-01-26 14:10:09 -0600

    menu creado

--- Lab2P2_AndreFlores_/src/lab2p2_andreflores_/Lab2P2_AndreFlores_.java
+++ Lab2P2_AndreFlores_/src/lab2p2_andreflores_/Lab2P2_AndreFlores_.java
@@ -1,10 +1,46 @@
-
 package lab2p2_andreflores_;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lab2P2_AndreFlores_ {
 
     public static void main(String[] args) {
-        
+        int opcion = 0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("  Menu principal \n"
+                    + " 1. Listar recursos\n"
+                    + " 2. Crear Recursos\n"
+                    + " 3. eliminar recursos\n"
+                    + " 4. modificar recurso\n"
+                    + " 5. salir\n"
+                    );
+        do {
+            opcion = entrada.nextInt();
+            
+            switch (opcion) {
+                case 1:
+                    System.out.println("Listar recursos");
+                    break;
+                case 2:
+                    System.out.println("Crear recursos");
+                    break;
+                case 3:
+                    System.out.println("eliminar recursos");
+                    break;
+                case 4:
+                    System.out.println("modificar recursos");
+                    break;
+                case 5:
+                    System.out.println("salir");
+                    break;
+                default:
+
+                    break;
+
+            } // fin switch
+
+        } while (opcion != 5);// fin do
     }
-    
+
 }
