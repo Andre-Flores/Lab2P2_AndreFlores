076a140f1d21e156f4b675b1ec2d7bf035d9c523
Andre-Flores <andrefloresromero@gmail.com>
2024-01-26 13:58:57 -0600

    clase articulos hecha

--- Lab2P2_AndreFlores_/src/lab2p2_andreflores_/Articulos.java
+++ Lab2P2_AndreFlores_/src/lab2p2_andreflores_/Articulos.java
@@ -9,4 +9,58 @@ public class Articulos {
     String TemaArt;
     Date fechaPublicacionArt;
     boolean accesoenLineaArt;
+
+    public Articulos(String tituloArt, String autorArt, String TemaArt, Date fechaPublicacionArt, boolean accesoenLineaArt) {
+        this.tituloArt = tituloArt;
+        this.autorArt = autorArt;
+        this.TemaArt = TemaArt;
+        this.fechaPublicacionArt = fechaPublicacionArt;
+        this.accesoenLineaArt = accesoenLineaArt;
+    }
+
+    public String getTituloArt() {
+        return tituloArt;
+    }
+
+    public String getAutorArt() {
+        return autorArt;
+    }
+
+    public String getTemaArt() {
+        return TemaArt;
+    }
+
+    public Date getFechaPublicacionArt() {
+        return fechaPublicacionArt;
+    }
+
+    public boolean isAccesoenLineaArt() {
+        return accesoenLineaArt;
+    }
+
+    public void setTituloArt(String tituloArt) {
+        this.tituloArt = tituloArt;
+    }
+
+    public void setAutorArt(String autorArt) {
+        this.autorArt = autorArt;
+    }
+
+    public void setTemaArt(String TemaArt) {
+        this.TemaArt = TemaArt;
+    }
+
+    public void setFechaPublicacionArt(Date fechaPublicacionArt) {
+        this.fechaPublicacionArt = fechaPublicacionArt;
+    }
+
+    public void setAccesoenLineaArt(boolean accesoenLineaArt) {
+        this.accesoenLineaArt = accesoenLineaArt;
+    }
+
+    @Override
+    public String toString() {
+        return "Articulos{" + "tituloArt=" + tituloArt + ", autorArt=" + autorArt + ", TemaArt=" + TemaArt + ", fechaPublicacionArt=" + fechaPublicacionArt + ", accesoenLineaArt=" + accesoenLineaArt + '}';
+    }
+    
 }
